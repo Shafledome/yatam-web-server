2fc0eb3785ae291bf5de6b2a6e10b72356c58216
gonzalo1398 <gonzalo1398@gmail.com>
2020-11-22 13:18:45 +0100

    User updated

--- src/app/entities/user.py
+++ src/app/entities/user.py
@@ -7,16 +7,17 @@ class User:
 
     def __init__(self, email, username=None, password=None):
         if username is None and password is None:
-            u = db.get('user', email)
+            u = db.get('users', email)
             self.email = email
             self.username = u['username']
             self.password = u['password']
         else:
-            db.create('user', email, {'username': username, 'password': password})
+            db.create('users', email, {'username': username, 'password': password})
             self.email = email
             self.username = username
             self.password = password
 
+    # Primary key doesn't have set method
     def get_email(self):
         return self.email
 
@@ -25,6 +26,34 @@ class User:
 
     def set_username(self, username):
         self.username = username
+        db.update('users', self.email, {'username': username})
+
+    def get_password(self):
+        return self.password
+
+    def set_password(self, password):
+        self.password = password
+        db.update('users', self.email, {'password': password})
+
+    @staticmethod
+    def get_dict():
+        return db.get('users')
+
+    @staticmethod
+    def search_by_email(email):
+        return db.get('users', email)
+
+    def search_by_username(username):
+        data = None
+        d = db.get('users')
+        for u in d:
+            data = d[u]
+            if data['username'] == username:
+                break
+            else:
+                data = None
+        return data
+
 
 
 
