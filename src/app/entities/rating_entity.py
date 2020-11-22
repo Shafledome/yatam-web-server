5dcac4451e039d36ceb22cc2d02268ba83044730
JuanSalmeronM <salmeron9812@gmail.com>
2020-11-22 20:00:33 +0100

    Updated Rating to the new dc.py methods and changed some spacing and indentation to follow PEP rules

--- src/app/entities/rating_entity.py
+++ src/app/entities/rating_entity.py
@@ -1,23 +1,23 @@
-import sys, os
+import os
+import sys
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import utilities.db as db
+
 
 class Rating:
 
     def __init__(self, rating_id, user_email=None, leisure_id=None):
         if user_email is None and leisure_id is None:
-            r = db.get('rating', id)
+            r = db.get_dict('rating', rating_id)
             self.id = rating_id
             self.user_email = r['user_email']
             self.leisure_id = r['leisure_id']
         else:
-            db.create('rating', rating_id, {'user_email': user_email, 'leisure_id': leisure_id})
+            db.create('rating', {'user_email': user_email, 'leisure_id': leisure_id})
             self.rating_id = rating_id
             self.user_email = user_email
             self.leisure_id = leisure_id
-
-    def get_rating_id(self):
-        return self.rating_id
 
     def get_user_email(self):
         return self.user_email
@@ -29,32 +29,22 @@ class Rating:
 
     def set_user_email(self, user_email):
         self.user_email = user_email
+        key = db.search_key('rating', 'user_email', self.user_email)
+        db.update('rating', key, {'user_email': user_email})
 
     def set_leisure_id(self, leisure_id):
         self.leisure_id = leisure_id
+        key = db.search_key('rating', 'leisure_id', self.leisure_id)
+        db.update('rating', key, {'leisure_id': leisure_id})
 
     @staticmethod
     def get_dict():
-        return db.get('rating')
+        return db.get_dict('rating')
 
+    @staticmethod
     def search_by_user(user_email):
-        data = None
-        d = db.get('rating')
-        for r in d:
-            data = d[r]
-            if data['user_email'] == user_email:
-                break
-            else:
-                data = None
-        return data
+        return db.search_values('rating', 'user_email', user_email)
 
+    @staticmethod
     def search_by_leisure(leisure_id):
-        data = None
-        d = db.get('rating')
-        for r in d:
-            data = d[r]
-            if data['leisure_id'] == leisure_id:
-                break
-            else:
-                data = None
-        return data
+        return db.search_values('rating', 'leisure_id', leisure_id)
