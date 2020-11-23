d3de524ec8388eec937e3adf6c49a7362cc46b46
Shafledome <shafledome@gmail.com>
2020-11-23 19:58:57 +0100

    Now db.py can connect to Firebase

--- src/app/utilities/db.py
+++ src/app/utilities/db.py
@@ -3,7 +3,15 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-config = os.getenv('FIREBASECONFIG')
+config = {'apiKey': os.getenv('apiKey'),
+          'authDomain': os.getenv('authDomain'),
+          'databaseURL': os.getenv('databaseURL'),
+          'projectId': os.getenv('projectId'),
+          'storageBucket': os.getenv('storageBucket'),
+          'messagingSenderId': os.getenv('messagingSenderId'),
+          'appId': os.getenv('appId'),
+          'measurementId': os.getenv('measurementId')
+          }
 
 firebase = pyrebase.initialize_app(config)
 
