8c01bc506e174a6fd24a1c21b6e3a41c69d047b6
gonzalo1398 <gonzalo1398@gmail.com>
2020-12-01 15:07:45 +0100

    Trophy is added as the materialization of trophies

---
+++ src/app/entities/trophy.py
@@ -0,0 +1,58 @@
+import os
+import sys
+import datetime
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import utilities.db as db
+
+
+class Trophy:
+    def __init__(self, key=None, trophy=None, user=None):
+        if key is not None and trophy is None and user is None:
+            t = db.search_by_key('trophies', key)
+            self.trophy = t['trophy']
+            self.user = t['user']
+            self.date = t['date']
+        elif key is None and trophy is not None and user is not None:
+            date = datetime.datetime.now().strftime("%c")
+            db.create('trophies', {'trophy': trophy, 'user': user, 'date': date})
+            self.trophy = trophy
+            self.user = user
+            self.date = date
+
+    def get_user_key(self):
+        return self.user
+
+    def get_trophy_key(self):
+        return self.trophy
+
+    def get_date(self):
+        return self.date
+
+    @staticmethod
+    def delete(key):
+        db.delete('trophy', key)
+
+    @staticmethod
+    def get_dict():
+        return db.get_dict('trophy')
+
+    @staticmethod
+    def search_by_key(key):
+        return db.search_by_key('trophy', key)
+
+    @staticmethod
+    def search_by_user(user):
+        return db.get_dict('trophies', 'user', user)
+
+    @staticmethod
+    def search_by_trophy(trophy):
+        return db.get_dict('trophies', 'trophy', trophy)
+
+    @staticmethod
+    def set_user(key, user):
+        db.update('trophies', key, {'user': user})
+
+    @staticmethod
+    def set_trophy(key, trophy):
+        db.update('trophies', key, {'trophy': trophy})
